bbec1a24176cd967eb2adeb48974323aeae00611
Jesse Herrick <jessegrantherrick@gmail.com>
2013-09-01 17:52:23 -0400

    Need to figure out how to unzip this now.

--- lib/bukkit/install.rb
+++ lib/bukkit/install.rb
@@ -1,19 +1,29 @@
 require 'open-uri'
 require 'json'
+require 'zip'
 
 module Bukkit
 	def self.install
 		opt2 = ARGV[1]
-#		if opt3.nil?
-#			abort("USAGE: bukkit install PLUGIN_NAME") 
-#		else
-#			nil
-#		end
+		if opt2.nil?
+			abort("USAGE: bukkit install PLUGIN_NAME")
+		end
+		begin
+			plugins_api = JSON.parse(open("http://api.bukget.org/3/plugins/bukkit/#{opt2}").read)
+			download = plugins_api["versions"][0]["download"]
+			filename = plugins_api["versions"][0]["filename"]
 
-		plugins_api = JSON.parse(open("http://api.bukget.org/3/plugins/bukkit/#{opt2}").read)
-		download = plugins_api["versions"][0]["download"]
-		filename = plugins_api["versions"][0]["filename"]
-
-		puts download
+			Dir.chdir("plugins")
+			%x(curl -L #{download} > #{filename})
+			if File.extname(filename) == ".zip"
+				puts "Extract! Extract!!!"
+			else
+				puts "It's not a zip! It's installed!"
+			end
+		rescue Errno::ENOENT
+			puts "  You aren't in a server's root directory.\n  'plugins' directory not found."
+		rescue
+			puts "  Plugin not found."
+		end
 	end
 end
