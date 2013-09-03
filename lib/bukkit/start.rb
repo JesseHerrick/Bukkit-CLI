090f8a04a9a627877fa43757e2c5b50a5d4abd33
Jesse Herrick <jessegrantherrick@gmail.com>
2013-09-02 22:15:31 -0400

    Added a new option for the new command.

--- lib/bukkit/start.rb
+++ lib/bukkit/start.rb
@@ -1,7 +1,13 @@
 module Bukkit
 	def self.start
-		puts "Make sure that you're in your server's root directory."
-		puts "If Java runtime is not present, run 'bukkit start' once Java is installed to complete the installation."
-		system 'java -jar craftbukkit.jar'
+		if ARGV.include? "-s"
+			abort
+		elsif ARGV.include? "-s"
+			abort
+		else
+			puts "Make sure that you're in your server's root directory."
+			puts "If Java runtime is not present, run 'bukkit start' once Java is installed to complete the installation."
+			system 'java -jar craftbukkit.jar'
+		end
 	end
 end
