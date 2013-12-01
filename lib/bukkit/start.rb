022bec6aad5c08aa347584bbeb65128c79cfa870
Jesse Herrick <jessegrantherrick@gmail.com>
2013-12-01 11:03:37 -0500

    v2.1.3: Fix some bugs.

--- lib/bukkit/start.rb
+++ lib/bukkit/start.rb
@@ -2,8 +2,12 @@ module Bukkit
 	class Server
 		# Start the server.
 		def self.start
-			puts "Starting your CraftBukkit server.".green
-			system 'java -jar craftbukkit.jar'
+			if File.exists?("craftbukkit.jar")
+				abort "You're not in your server's root directory.".red
+			else
+				puts "Starting your CraftBukkit server.".green
+				system 'java -jar craftbukkit.jar'
+			end
 		end
 	end
 end
--- lib/bukkit/version.rb
+++ lib/bukkit/version.rb
@@ -1,4 +1,4 @@
 module Bukkit
-	VERSION = "2.1.2"
+	VERSION = "2.1.3"
 	VERSION_FULL = "Bukkit-CLI v#{VERSION}"
 end
