746ca3930c0965453b3afe96bf8c2508340e7164
JesseHerrick <jessegrantherrick@gmail.com>
2014-01-06 00:46:44 -0500

    Begin feature tests for install command.

--- features/create.feature
+++ features/create.feature
@@ -4,5 +4,5 @@ Feature: Create Server
 	I want to be able to create a new server easily.
 
 	Scenario: Create a new server
-		When I run "bukkit new MC" with the options "--rb --no-start"
+		When I run "bukkit new MC" with the options "--rb --nostart"
 		Then the file "MC/craftbukkit.jar" should exist
--- features/step_definitions/bukkit_steps.rb
+++ features/step_definitions/bukkit_steps.rb
@@ -1,7 +1,11 @@
 When(/^I run "(.*?)" with the options "(.*?)"$/) do |command, options|
-	@output = `#{command} #{options}`
+  @output = `#{command} #{options}`
 end
 
 Then(/^the exit status should be (\d+)$/) do |exit_status|
-	@last_exit_status.should == exit_status.to_i
+  @last_exit_status.should == exit_status.to_i
+end
+
+Then(/^the file "(.*?)" should exist$/) do |file|
+  File.exists?(file)
 end
