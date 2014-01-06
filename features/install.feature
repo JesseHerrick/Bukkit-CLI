ff80595a929fc495dd081a7d5ef74a77995f1c49
JesseHerrick <jessegrantherrick@gmail.com>
2014-01-06 02:53:47 -0500

    Install command tests working with simulated servers.

--- features/install.feature
+++ features/install.feature
@@ -4,12 +4,12 @@ Feature: Install
 	I want to install a new plugin.
 
 	Scenario: Install WorldEdit
-		Given a new server "MC"
-		When "WorldEdit" is installed
+		Given a simulated server "MC"
+		When I run `bukkit install WorldEdit`
 		Then the file "MC/plugins/WorldEdit.jar" should exist
 		And the exit code should be 0
 
 	Scenario: Install an unknown plugin
-		Given a new server "MC"
-		When "jesse" is installed
+		Given a simulated server "MC"
+		When I run `bukkit install jesse`
 		Then the exit code should be 1
--- features/step_definitions/bukkit_steps.rb
+++ features/step_definitions/bukkit_steps.rb
@@ -2,14 +2,21 @@ Before do
   @aruba_timeout_seconds = 85
 end
 
-When(/^I run "(.*?)" with the options "(.*?)"$/) do |command, options|
-  @output = `#{command} #{options}`
-end
-
 Then(/^the exit code should be (\d+)$/) do |exit_status|
   @last_exit_status.should == exit_status.to_i
 end
 
 Then(/^the file "(.*?)" should exist$/) do |file|
   File.exists?(file)
+end
+
+Given(/^a simulated server "(.*?)"$/) do |name|
+  Dir.chdir("tmp")
+  Dir.mkdir("MC")
+  Dir.chdir("MC")
+  Dir.mkdir("plugins")
+
+  file = File.new("craftbukkit.jar", "w")
+  file.write("This is just a simulated jarfile.")
+  file.close
 end
